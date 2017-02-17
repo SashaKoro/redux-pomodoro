2ad2e8ca42669220ed69cfc1b5c31204c380b9f9
SashaKoro <balaskorotkov@gmail.com>
2017-02-16 20:42:02 -0500

    concise tests

--- __tests__/task_break_test.js
+++ __tests__/task_break_test.js
@@ -5,79 +5,72 @@ import { shallow, mount } from 'enzyme';
 import TaskBreak from '../src/components/task_break';
 
 describe('Task_Break', () => {
+  const props = {
+    taskTime: 25,
+    breakTime: 5,
+    taskModify: jest.fn(),
+    breakModify: jest.fn(),
+    ifRunning: false,
+  };
+
   it('should be selectable by class name', () => {
-    const wrapper = shallow(
-      <TaskBreak
-        taskTime={25}
-        breakTime={5}
-        taskModify={jest.fn()}
-        breakModify={jest.fn()}
-        ifRunning={false}
-      />);
+    const wrapper = shallow(<TaskBreak {...props} />);
     expect(wrapper.is('.Task_Break')).toBe(true);
   });
 
   it('should mount to a DOM', () => {
-    const wrapper = mount(
-      <TaskBreak
-        taskTime={25}
-        breakTime={5}
-        taskModify={jest.fn()}
-        breakModify={jest.fn()}
-        ifRunning={false}
-      />);
+    const wrapper = mount(<TaskBreak {...props} />);
     expect(wrapper.find('.Task_Break').length).toBe(1);
   });
 
-  const wrapper = mount(
-    <TaskBreak
-      taskTime={25}
-      breakTime={5}
-      taskModify={jest.fn()}
-      breakModify={jest.fn()}
-      ifRunning
-    />);
-
   it('should disable buttons from ifRunning prop', () => {
+    let runningProps = { ...props, ifRunning: true };
+    const wrapper = mount(<TaskBreak {...runningProps} />);
+
     expect(wrapper.find('.subtractTask').prop('disabled')).toBe(true);
     expect(wrapper.find('.addTask').prop('disabled')).toBe(true);
     expect(wrapper.find('.subtractBreak').prop('disabled')).toBe(true);
     expect(wrapper.find('.addBreak').prop('disabled')).toBe(true);
   });
   it('should display correct task and break times from props', () => {
+    let runningProps = { ...props, ifRunning: true };
+    const wrapper = mount(<TaskBreak {...runningProps} />);
+
     expect(wrapper.find('.taskDisplay').text()).toBe('25');
     expect(wrapper.find('.breakDisplay').text()).toBe('5');
   });
 
-  let taskModifyMock = jest.fn();
-  let breakModifyMock = jest.fn();
+  // let taskModifyMock = jest.fn();
+  // let breakModifyMock = jest.fn();
 
-  const newWrapper = mount(
-    <TaskBreak
-      taskTime={25}
-      breakTime={5}
-      taskModify={taskModifyMock}
-      breakModify={breakModifyMock}
-      ifRunning={false}
-    />);
+  // const newWrapper = mount(
+  //   <TaskBreak
+  //     taskTime={25}
+  //     breakTime={5}
+  //     taskModify={jest.fn()}
+  //     breakModify={jest.fn()}
+  //     ifRunning={false}
+  //   />);
+  const wrapper = mount(<TaskBreak {...props} />);
 
   it('expects clicking subtractTask to call taskModify func with args', () => {
-    newWrapper.find('.subtractTask').simulate('click');
-    expect(taskModifyMock.mock.calls[0][0]).toBe(-1);
+
+    wrapper.find('.subtractTask').simulate('click');
+    expect(props.taskModify.mock.calls[0][0]).toBe(-1);
   });
 
   it('expects clicking addTask to call taskModify func with args', () => {
-    newWrapper.find('.addTask').simulate('click');
-    expect(taskModifyMock.mock.calls[1][0]).toBe(1);
+    wrapper.find('.addTask').simulate('click');
+    expect(props.taskModify.mock.calls[1][0]).toBe(1);
   });
 
   it('expects clicking subtractBreak to call breakModify func with args', () => {
-    newWrapper.find('.subtractBreak').simulate('click');
-    expect(breakModifyMock.mock.calls[0][0]).toBe(-1);
+    wrapper.find('.subtractBreak').simulate('click');
+    expect(props.breakModify.mock.calls[0][0]).toBe(-1);
   });
 
   it('expects clicking addBreak to call breakModify func with args', () => {
-    newWrapper.find('.addBreak').simulate('click');
-    expect(breakModifyMock.mock.calls[1][0]).toBe(1);
+    wrapper.find('.addBreak').simulate('click');
+    expect(props.breakModify.mock.calls[1][0]).toBe(1);
   });
 });
--- __tests__/timerButtons_test.js
+++ __tests__/timerButtons_test.js
@@ -5,60 +5,42 @@ import { shallow, mount } from 'enzyme';
 import TimerButtons from '../src/components/timerButtons';
 
 describe('TimerButtons', () => {
+  const props = {
+    start: jest.fn(),
+    pause: jest.fn(),
+    ifRunning: true,
+  };
+
   it('is selectable by class name', () => {
-    const wrapper = shallow(
-      <TimerButtons
-        start={jest.fn()}
-        pause={jest.fn()}
-        ifRunning
-      />);
+    const wrapper = shallow(<TimerButtons {...props} />);
     expect(wrapper.is('.TimerButtons')).toBe(true);
   });
 
   it('should mount to a DOM', () => {
-    const wrapper = mount(
-      <TimerButtons
-        start={jest.fn()}
-        pause={jest.fn()}
-        ifRunning
-      />);
+    const wrapper = mount(<TimerButtons {...props} />);
     expect(wrapper.find('.TimerButtons').length).toBe(1);
   });
 
+  let notRunningProps = { ...props, ifRunning: false };
+
   it('should disable buttons from ifRunning prop', () => {
-    const wrapper = mount(
-      <TimerButtons
-        start={jest.fn()}
-        pause={jest.fn()}
-        ifRunning={false}
-      />);
+    const wrapper = mount(<TimerButtons {...notRunningProps} />);
+
     expect(wrapper.find('.start').prop('disabled')).toBe(false);
     expect(wrapper.find('.pause').prop('disabled')).toBe(true);
   });
 
   it('should call the start() function on start button click', () => {
-    let startMock = jest.fn();
-    let pauseMock = jest.fn();
-    const wrapper = mount(
-      <TimerButtons
-        start={startMock}
-        pause={pauseMock}
-        ifRunning={false}
-      />);
+    const wrapper = mount(<TimerButtons {...notRunningProps} />);
+
     wrapper.find('.start').simulate('click');
-    expect(startMock.mock.calls.length).toBe(1);
+    expect(props.start.mock.calls.length).toBe(1);
   });
 
   it('should call the pause() function on pause button click', () => {
-    let startMock = jest.fn();
-    let pauseMock = jest.fn();
-    const wrapper = mount(
-      <TimerButtons
-        start={startMock}
-        pause={pauseMock}
-        ifRunning
-      />);
+    const wrapper = mount(<TimerButtons {...props} />);
+    
     wrapper.find('.pause').simulate('click');
-    expect(pauseMock.mock.calls.length).toBe(1);
+    expect(props.pause.mock.calls.length).toBe(1);
   });
 });
